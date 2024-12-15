09724f7fcc25845025546a79613a346cddff1833
Tabish Akhtar <tabishakhtar007860@gmail.com>
2024-12-15 18:37:02 +0500

    added clinet

--- clinet/src/app/page.tsx
+++ clinet/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Landing from "../pages/Landing";
-
+import Login from "../components/Login";
 
 export default function Home() {
   return (
     <>
       <Landing />
+      {/* <Login /> */}
     </>
   );
 }
