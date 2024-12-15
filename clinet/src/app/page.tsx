d5e4c5825036f9894c099bf32871e7a2a33324ff
Tabish Akhtar <tabishakhtar007860@gmail.com>
2024-12-15 18:48:30 +0500

    corrected deploy issue

--- clinet/src/app/page.tsx
+++ clinet/src/app/page.tsx
@@ -1,11 +1,10 @@
 import Landing from "../pages/Landing";
-import Login from "../components/Login";
 
 export default function Home() {
   return (
     <>
       <Landing />
-      {/* <Login /> */}
+      
     </>
   );
 }
